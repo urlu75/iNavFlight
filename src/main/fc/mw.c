57908fc63cfc4d79e9a0016422ae29b867ae898d
Konstantin (DigitalEntity) Sharlaimov <konstantin.sharlaimov@gmail.com>
2016-07-23 14:37:34 +0300

    Auto arm fixed wing aircrafts (improved)

--- src/main/fc/mw.c
+++ src/main/fc/mw.c
@@ -337,7 +337,7 @@ void processRx(void)
         }
     }
 
-    processRcStickPositions(&masterConfig.rxConfig, throttleStatus, masterConfig.disarm_kill_switch);
+    processRcStickPositions(&masterConfig.rxConfig, throttleStatus, masterConfig.disarm_kill_switch, masterConfig.fixed_wing_auto_arm);
 
     updateActivatedModes(currentProfile->modeActivationConditions, currentProfile->modeActivationOperator);
 
